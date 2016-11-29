f31076bc75d622dfec0e46d524b39f5551e421aa
dannycrews <dancrews@me.com>
2016-11-29 18:09:03 -0500

    Install rspec and capyara

--- app/models/conjur.rb
+++ app/models/conjur.rb
@@ -4,12 +4,26 @@ class Conjur < ApplicationRecord
   default_timeout 1 #hard timeout after 1 second
 
   def self.handle_timeouts
-      begin
-        yield
-      rescue Net::OpenTimeout, Net::ReadTimeout
-        {}
-      end
+    begin
+      yield
+    rescue Net::OpenTimeout, Net::ReadTimeout
+      {}
     end
+  end
+
+  # def cache_key
+  #   "conjurmaster"
+  # end
+
+  # def handle_caching(options)
+  #   if cached = REDIS.get(cache_key)
+  #     JSON[cached]
+  #   else
+  #     yield.tap do |results|
+  #       REDIS.setex(cache_key, 10000, results.to_json)
+  #     end
+  #   end
+  # end
 
   def self.get_health
     handle_timeouts do
