a7b6e7b0610b11f1b3f1a2124820858491d4ff2e
dannycrews <dancrews@me.com>
2016-12-06 11:03:28 -0500

    Add initial ConjurApp class test

---
+++ spec/resources/conjur_api_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+require 'conjur_api'
+
+describe ConjurApi do
+
+  describe '.get_health' do
+    it 'returns a health status object' do
+      response = ConjurApi.get_health
+
+      expect(response).to be_an_instance_of(Hash)
+      expect(response['database']['replication_status']["pg_stat_replication"]).
+            to be_an_instance_of(Array)
+      response.size.should be  > 0
+    end
+
+  end
+
+end
