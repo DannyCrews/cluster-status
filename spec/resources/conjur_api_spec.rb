135fc578f2d7f2a253f03b55c5fe0ea7063de8c6
dannycrews <dancrews@me.com>
2016-12-06 11:59:21 -0500

    Add VCR

--- spec/resources/conjur_api_spec.rb
+++ spec/resources/conjur_api_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 require 'conjur_api'
 
-describe ConjurApi do
+  describe ConjurApi do
 
-  describe '.get_health' do
-    it 'returns a health status object' do
-      response = ConjurApi.get_health
+    describe '.get_health' do
+      it 'returns a health status object' do
+        VCR.use_cassette('conjur_health') do
+          response = ConjurApi.get_health
 
-      expect(response).to be_an_instance_of(Hash)
-      expect(response['database']['replication_status']["pg_stat_replication"]).
-            to be_an_instance_of(Array)
-      response.size.should be  > 0
+          expect(response).to be_an_instance_of(Hash)
+          expect(response['database']['replication_status']["pg_stat_replication"]).
+              to be_an_instance_of(Array)
+          response.size.should be  > 0
+        end
+      end
+
     end
 
   end
-
-end
