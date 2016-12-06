8f4489e7f96a1aa4f62ed8b453c57cdba2960c25
dannycrews <dancrews@me.com>
2016-12-06 12:22:23 -0500

    Break up spec examples

--- spec/resources/conjur_api_spec.rb
+++ spec/resources/conjur_api_spec.rb
@@ -4,17 +4,22 @@ require 'conjur_api'
   describe ConjurApi do
 
     describe '.get_health' do
-      it 'returns a health status object' do
-        VCR.use_cassette('conjur_health') do
-          response = ConjurApi.get_health
+      VCR.use_cassette('conjur_health') do
+        response = ConjurApi.get_health
 
+        it 'returns a health status object' do
+            expect(response.size).to be  > 0
+          end
+
+        it 'returns a hash' do
           expect(response).to be_an_instance_of(Hash)
+        end
+
+        it 'returns a "pg_stat_replication" array' do
           expect(response['database']['replication_status']["pg_stat_replication"]).
               to be_an_instance_of(Array)
-          expect(response.size).to be  > 0
         end
       end
-
     end
 
   end
