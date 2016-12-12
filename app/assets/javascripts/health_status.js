edb8cca27cf822aada59c4f8fd67638e0c5139b1
dannycrews <dancrews@me.com>
2016-12-12 11:17:02 -0500

    refactor document.ready function

--- app/assets/javascripts/health_status.js
+++ app/assets/javascripts/health_status.js
@@ -1,11 +1,8 @@
-$(document).ready(function() {
-    var currentUrl = window.location.href;
-
-
-    setInterval(function() {
-        $.ajax({
-            type: "GET",
-            url: "/health/health_status"
-        });
-    }, 10000);
+$(function() {
+  setInterval(function() {
+      $.ajax({
+          type: "GET",
+          url: "/health/health_status"
+      });
+  }, 10000);
 });
